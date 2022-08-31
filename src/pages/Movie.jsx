38587dd68d9293f1f668a9dc5e52c061aad3ede3
Renan <renan.fachin@hotmail.com>
2022-08-31 16:34:03 -0300

    Implementa as funcionalidades da página de detalhes

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -6,7 +6,7 @@ import { useParams } from 'react-router-dom'
 import MovieCard from '../components/MovieCard/'
 
 import {
-    BsGrapghUp,
+    BsGraphUp,
     BsWallet2,
     BsHourglassSplit,
     BsFillFileEarmarkTextFill
@@ -22,6 +22,14 @@ const Movie = () => {
     // pegando o id da url
     const {id} = useParams()
     const [movie,setMovie] = useState(null)
+
+    // função para conversão de unidades
+    const formatCurrency = (number) =>{
+        return number.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD"
+        })
+    }
 
         // Função para buscar na API
         const getMovie = async(url) =>{
@@ -39,8 +47,44 @@ const Movie = () => {
         },[])
 
     return(
-        <div>Movie</div>
-    )
+        <div className="movie-page">
+            {movie && (
+                <>
+                <MovieCard movie={movie} showLink={false} />
+                <p className="tagline"></p>
+
+                <div className="info">
+                    <h3>
+                        <BsWallet2/> Orçamento:
+                    </h3>
+                    <p>{formatCurrency(movie.budget)}</p>
+                </div>
+
+                <div className="info">
+                    <h3>
+                        <BsGraphUp/> Receita:
+                    </h3>
+                    <p>{formatCurrency(movie.revenue)}</p>
+                </div>
+
+                <div className="info">
+                    <h3>
+                        <BsHourglassSplit/> Duração:
+                    </h3>
+                    <p>{movie.runtime} minutos</p>
+                </div>
+
+                <div className="info">
+                    <h3>
+                        <BsFillFileEarmarkTextFill/> Descrição:
+                    </h3>
+                    <p>{movie.overview}</p>
+                </div>
+
+                </>
+            )}
+        </div>
+    );
 }
 
 export default Movie
