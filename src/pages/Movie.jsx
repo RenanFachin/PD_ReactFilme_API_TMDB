be81a99aca569abe2622d87f84f25eff2f19b03c
Renan <renan.fachin@hotmail.com>
2022-08-31 16:24:29 -0300

    Cria os states e useEffects para a página de detalhes

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -19,6 +19,25 @@ const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
 const Movie = () => {
+    // pegando o id da url
+    const {id} = useParams()
+    const [movie,setMovie] = useState(null)
+
+        // Função para buscar na API
+        const getMovie = async(url) =>{
+            const res = await fetch(url)
+            const data = await res.json()
+    
+            setMovie(data) // sem o results pq vai ser só 1 filme retornando
+        }
+
+        useEffect(()=>{
+            // Montando a URL
+            const movieUrl = `${moviesURL}${id}?${apiKey}`
+
+            getMovie(movieUrl)
+        },[])
+
     return(
         <div>Movie</div>
     )
