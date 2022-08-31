ddfeda1af10969646fc39d0ac57bc1afd95854f1
Renan <renan.fachin@hotmail.com>
2022-08-31 14:48:07 -0300

    Definida algumas estilizações globais do projeto e inicio da criação do componente NAVBAR

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+
+import Navbar from './components/Navbar/'
 
 function App() {
   return (
     <div className="App">
-      <nav id="navbar">
-        <h2>
-          <Link to="/">MoviesLib</Link>
-        </h2>
-        <Link to="/movie/1">Movies</Link>
-        <Link to="/search">Search</Link>
-
-      </nav>
-      
+      <Navbar />      
       <h2>Movies Lib</h2>
 
       <Outlet />
